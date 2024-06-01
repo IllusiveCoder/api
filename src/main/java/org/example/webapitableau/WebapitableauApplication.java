492a7096f50dd9c3289fb9b75620584bc286063d
Illusive Coder <57768405+IllusiveCoder@users.noreply.github.com>
2024-06-01 17:48:35 +0200

    Database communciation for outside now possible.

--- src/main/java/org/example/webapitableau/WebapitableauApplication.java
+++ src/main/java/org/example/webapitableau/WebapitableauApplication.java
@@ -2,13 +2,20 @@ package org.example.webapitableau;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebapitableauApplication {
+public class WebapitableauApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(WebapitableauApplication.class, args);
 
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WebapitableauApplication.class);
     }
 
 }
