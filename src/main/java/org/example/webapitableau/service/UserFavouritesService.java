206220dcb3de73b523d47da2920dd40333584b44
Illusive Coder <57768405+IllusiveCoder@users.noreply.github.com>
2024-06-05 01:54:40 +0200

    Updated methods.

--- src/main/java/org/example/webapitableau/service/UserFavouritesService.java
+++ src/main/java/org/example/webapitableau/service/UserFavouritesService.java
@@ -9,13 +9,16 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.awt.print.Pageable;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
+
+
 
 @Service
 public class UserFavouritesService {
 
+
+    public HashMap<String, List<Integer>> hashMap = new HashMap<>();
     @Autowired
     private RecipeRepository recipeRepository;
 
@@ -24,36 +27,40 @@ public class UserFavouritesService {
 
     @Autowired
     private RecipeMapper recipeMapper;
+    @Autowired
+    private RecipeService recipeService;
 
     public PaginatedFavourites deletefavourite(String uid, Integer recipeid) {
+        List<Integer> favourites = hashMap.get(uid);
+        favourites.remove(recipeid);
+        hashMap.put(uid, favourites);
+
         return null;
     }
 
     public PaginatedFavourites getfavourites(String uid) {
-//        String favs = userFavouritesRepository.findByUid(uid);
-//        String[] favsList = favs.split(",");
-//        List<String> ids = Arrays.asList(favsList);
-//        List<Integer> idsInt = ids.stream().map(Integer::parseInt).collect(Collectors.toList());
-//        List<RecipeEntity> recipeEntityList = recipeRepository.findAllById(idsInt);
-//        List<RecipeShort> recipeShortList = RecipeMapper.mapRecipeEntitiesToRecipeShorts(recipeEntityList);
-//        PaginatedFavourites paginatedFavourites = new PaginatedFavourites();
-//        paginatedFavourites.setContent(recipeShortList);
 
-        return  null;
+        List<Integer> favourites = hashMap.get(uid);
+        List<Recipe> recipeList = new ArrayList<>();
+        for (Integer favourite : favourites) {
+            recipeList.add(recipeService.getRecipe(favourite));
+        }
+        List<RecipeShort> List = RecipeMapper.mapRecipesToRecipeShorts(recipeList);
+        PaginatedFavourites pf = new PaginatedFavourites();
+        pf.setContent(List);
+        return  pf;
     }
 
     public PaginatedFavourites postfavourites(String uid, Integer recipeid) {
-//        List<UserFavouriteEntity> userFavouriteEntities = userFavouritesRepository.findBy(uid);
-//        UserFavouriteEntity userFavouriteEntity = userFavouriteEntities.get(0);
-//        String newfavs = userFavouriteEntity.getUid();
-//        if(newfavs.isEmpty()){
-//            newfavs = recipeid.toString() + ",";
-//        }
-//        else{
-//            newfavs = recipeid.toString() + ",";
-//        }
-//        userFavouriteEntity.setUid(newfavs);
-//        userFavouritesRepository.save(userFavouriteEntity);
+        if (!hashMap.containsKey(uid)) {
+            List<Integer> favourites = new ArrayList<>();
+            favourites.add(recipeid);
+            hashMap.put(uid, favourites);
+        }else{
+            List<Integer> favourites = hashMap.get(uid);
+            favourites.add(recipeid);
+            hashMap.put(uid, favourites);
+        }
         return null;
     }
 }
