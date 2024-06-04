a0f095e081b4593865211e8ae2fd9fbd12e2092b
Illusive Coder <57768405+IllusiveCoder@users.noreply.github.com>
2024-06-04 23:35:19 +0200

    Updated methods.

--- src/main/java/org/example/webapitableau/repository/RecipeRepository.java
+++ src/main/java/org/example/webapitableau/repository/RecipeRepository.java
@@ -1,5 +1,4 @@
 package org.example.webapitableau.repository;
-import org.example.webapitableau.models.Recipe;
 import org.example.webapitableau.models.RecipeEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,4 +11,5 @@ import java.util.List;
 public interface RecipeRepository extends JpaRepository<RecipeEntity, Integer> {
     Page<RecipeEntity> findByTitleContaining(String title, Pageable pageable);
     Page<RecipeEntity> findByCreatedby(String uid, Pageable pageable);
+    //List<RecipeEntity> findAllBy(List<Integer> ids, Pageable pageable);
 }
